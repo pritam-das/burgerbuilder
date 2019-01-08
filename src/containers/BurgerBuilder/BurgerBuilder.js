06882286eee910d5d625cde41186ecc9e9d03094
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-08 12:06:19 -0500

    Connected the checkout button in cart to actually send POST request to firebase database through axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders.js';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -79,7 +80,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-      alert('You continue!');
+      // alert('You continue!');
+      const order = {
+          ingredients: this.state.ingredients,
+          price: this.state.price,
+          customer: {
+              name: 'Pritam Das',
+              address: {
+                  street: 'W S E',
+                  zipcode: 'M1',
+                  country: 'Panama'
+              },
+              email: 'pdpdpd@gmail'
+          },
+          deliveryMethod: 'fastest'
+      }
+      axios.post('/orders.json', order)
+          .then((response) => {
+              console.log(response);
+          }).catch(error => console.log(error))
+
     }
 
     render () {
@@ -89,8 +109,6 @@ class BurgerBuilder extends Component {
       for (let key in disabledInfo) {
         disabledInfo[key] = disabledInfo[key] <= 0;
       }
-
-      console.log(disabledInfo);
       return (
         <Aux>
           <Modal show={this.state.purchasing}
